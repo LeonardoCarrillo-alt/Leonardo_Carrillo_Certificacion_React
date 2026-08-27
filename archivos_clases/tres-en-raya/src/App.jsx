064d52160d7140adbd4bbc5110d18c667182cf0b
LeonardoCarrillo-alt <leo.c.c.zubieta@gmail.com>
2026-08-27 18:15:42 -0400

    .

--- archivos_clases/tres-en-raya/src/App.jsx
+++ archivos_clases/tres-en-raya/src/App.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import TresEnRaya from './TresEnRaya.jsx'
 import './App.css'
-const contenedor = document.getElementById('root')
-  const root = createRoot(contenedor)
 
-  root.render(
-      <TresEnRaya />
-  )
+export default function App(){
+  const contenedor = document.getElementById('root')
+    const root = createRoot(contenedor)
+
+    root.render(
+        <TresEnRaya />
+    )
+}
